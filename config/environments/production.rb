32c4bad2475e36a340d26e234db2b0bff24aa46d
Tai Yin Michae Yim <mikeyimmy@gmail.com>
2014-09-10 15:39:16 +0800

    production ENV fb key added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  ENV['FACEBOOK_SECRET_ID'] = "e8dc98ac138c84c2b7dbf18e9f8a426b"
+
 end
